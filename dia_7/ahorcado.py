c0e394b6e2c11d8535dcb39d4bab65b63a2d33a8
Wilson Gutierrez Lancheros <wilsongutierrezl@gmail.com>
2021-01-17 01:02:10 -0500

    Ahora el juego finaliza solo hasta que se termine de adivinar toda la palabra para poder decirle al usuario que ganó.

--- dia_7/ahorcado.py
+++ dia_7/ahorcado.py
@@ -24,14 +24,19 @@ for _ in range(word_length):
 print('La solución es:',chosen_word)
 print(ahorcado)
 # 3. Solicitar adivinar una de las letras y asegurarse de que sea minúscula
-guess = input('Adivina una letra\n').lower()
+#    El juego debe finalizar solo cuando no hayan espacios, osea cuando se adivine
+#    la palabra completa.
+while '_' in ahorcado:
+  guess = input('Adivina una letra\n').lower()
 # 4. Revisar si la la letra se encuentra dentro de la palabra que se desea adivinar.
 #    Si el usuario adivina debe asignar la letra en la posición correcta y en caso 
 #    contrario colocar '_' por cada letra que no acierte.
-print("=====Resultados========")
-for position in range(word_length):
-  letter = chosen_word[position]
-  if letter == guess:
-    ahorcado[position] = letter     
-print(ahorcado)
-print("=======================")
+  print("=====Resultados========")
+  for position in range(word_length):
+    letter = chosen_word[position]
+    if letter == guess:
+      ahorcado[position] = letter     
+  print(ahorcado)
+  print("=======================")
+else:
+  print('You Win')
