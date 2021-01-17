b82e58e18f474cada4b0f8877bbd688afafc9a97
Wilson Gutierrez Lancheros <wilsongutierrezl@gmail.com>
2021-01-17 00:10:02 -0500

    Se agrega la visualización de los espacios en blanco que representa cada una de las letras de la palabra que se debe adivinar.

--- dia_7/ahorcado.py
+++ dia_7/ahorcado.py
@@ -13,19 +13,25 @@ print('''
 ahorcado=[]
 # 1. Crear una lista de palabras para adivinar
 word_list=['trabajando', 'biblioteca', 'fabrica']
-# 2. Seleccionar aleatoriamente una palabra de la lista anterior
+# 2. Seleccionar aleatoriamente una palabra de la lista anterior.
+#    Se valida el tamaño de la palabra y se agregan los '_' dentro de la variable
+#    ahorcado para visualizar de cuantas letras se compone la palabra.
 chosen_word = r.choice(word_list)
+word_length = len(chosen_word)
+
+for _ in range(word_length):
+  ahorcado += '_'
 print('La solución es:',chosen_word)
+print(ahorcado)
 # 3. Solicitar adivinar una de las letras y asegurarse de que sea minúscula
 guess = input('Adivina una letra\n').lower()
 # 4. Revisar si la la letra se encuentra dentro de la palabra que se desea adivinar.
 #    Si el usuario adivina debe asignar la letra en la posición correcta y en caso 
 #    contrario colocar '_' por cada letra que no acierte.
 print("=====Resultados========")
-for letter in chosen_word:
+for position in range(word_length):
+  letter = chosen_word[position]
   if letter == guess:
-    ahorcado.append(letter)
-  else:
-    ahorcado.append('_')
+    ahorcado[position] = letter     
 print(ahorcado)
 print("=======================")
