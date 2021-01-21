b47ea10edbcde78cf7ac2c1c677fdd8097c3b4a5
Wilson Gutierrez Lancheros <wilsongutierrezl@gmail.com>
2021-01-21 01:13:09 -0500

    Se agrega una cabecera al inicio de la ejecución del programa usando un modulo local y se corrige un bug que causaba error cuando el número de cambio era mayor que la cantidad unica de letras del alfabeto. Tambien se agrega la opción de que el usuario elija si quiere volver a intentar cifrar otro mensaje

--- dia_8/caesar_cipher_1/main.py
+++ dia_8/caesar_cipher_1/main.py
@@ -1,3 +1,6 @@
+from art import logo
+
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 
             'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 
             'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 
@@ -16,7 +19,18 @@ def caesar (choice_direction, text_send, number_shift):
       cipher_text += text_send[i]
   print(f'The {choice_direction}d text is: {cipher_text}')
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-caesar(direction, text, shift)  
+print(logo)
+
+should_end = False
+
+while not should_end:
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
+  shift %= 26
+  caesar(direction, text, shift)  
+
+  restart = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n")
+  if restart == "no":
+    should_end = True
+    print("Bye")
