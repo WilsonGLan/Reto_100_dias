340ce4a25459b65e90a97346045c4b6556b50801
Wilson Gutierrez Lancheros <wilsongutierrezl@gmail.com>
2021-01-25 07:16:06 -0500

    Se modifica el codigo para que no finalice la calculadora haciendo recursivo el llamado a la función que contiene todo el codigo

--- dia_10/Project_10/main_1.py
+++ dia_10/Project_10/main_1.py
@@ -27,25 +27,29 @@ operations = {
     "/": divide
 }
 
-num1 = int(input("What's the first number?: "))
+def calculator():
+    num1 = int(input("What's the first number?: "))
 
-for symbol in operations:
-    print(symbol)
+    for symbol in operations:
+        print(symbol)
 
-more_operations = True
+    more_operations = True
 
-while more_operations:
-    operation_symbol = input("Pick an operation from the line above: ")
-    num2 = int(input("What's the next number?: "))
+    while more_operations:
+        operation_symbol = input("Pick an operation from the line above: ")
+        num2 = int(input("What's the next number?: "))
 
-    calculation_function = operations[operation_symbol]
-    answer = calculation_function(num1, num2)
+        calculation_function = operations[operation_symbol]
+        answer = calculation_function(num1, num2)
 
-    print(f"{num1} {operation_symbol} {num2} = {answer}")
-    choice_operation = input(f"Type 'y' to continue calculation with {answer} or 'n' for exit: ")
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+        choice_operation = input(f"Type 'y' to continue calculation with {answer} or 'n' for exit: ")
 
-    if choice_operation == 'y':
-        num1 = answer
-    else:
-        more_operations = False
+        if choice_operation == 'y':
+            num1 = answer
+        else:
+            more_operations = False
+            calculator()
+
+calculator()
 
