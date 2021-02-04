c113d4142913b070e6dca6becfbc36729c2ae619
Wilson Gutierrez Lancheros <wilsongutierrezl@gmail.com>
2021-02-04 04:53:46 -0500

    Se modifica la lógica, calcula mcm y mcd

--- dia_13/exercise_01.py
+++ dia_13/exercise_01.py
@@ -1,41 +1,36 @@
-primos_numero_uno = []
-primos_numero_dos = []
+import menu_01 as opc
 
-#Descomponer los numeros ingresados en factores primos
+def encontrar_mcm(m,n):
+    m_max=m
+    n_max=n
+    mcm = 0
+    while m_max != n_max:
+        if n_max < m_max:
+            n_max += n
+        elif m_max < n_max:
+            m_max += m
+    if m_max == n_max:
+        mcm = m_max
+    return mcm
 
-def evaluar_primos(numero):            
-    for evalua_primo in range(2,numero):
-        if numero > 3 and numero % evalua_primo == 0:            
-            return False                        
-    return True
+def encontrar_mcd(m,n):
+    mcm = encontrar_mcm(m,n)
+    mcd = m/(mcm/n)
+    return mcd
 
-def lista_primos(numero):
-    for i in range(2, numero_uno):
-        if numero_uno >= 2 and numero_uno % i == 0 :
-            if i in [2,3]:
-                primos_numero_uno.append(i)
-            elif evaluar_primos(i):
-                primos_numero_uno.append(i)
-    return primos_numero_uno
-
-print("Calculadora de Minimo Común Multiplo y Mínimo Común Divisor")
-print("""
-=========================================
-||      MENU                           ||
-=========================================
-|| Opción || Operación                 ||
-|| 1.     || Mínimo Común Múltiplo     ||
-|| 2.     || Máximo Común Divisor      ||
-||        ||                           ||
-=========================================
-"""
-)
+print(opc.menu)
 
 opcion = int(input("Que opción del menú de operaciones selecciona:\t"))
 if opcion in [1,2]:
     numero_uno = int(input("Ingrese el primer número\t"))    
-    #numero_dos = int(input("Ingrese el segundo número\t"))     
-    print(lista_primos(numero_uno))   
+    numero_dos = int(input("Ingrese el segundo número\t"))
+    
+    if opcion == 1:     
+        m_c_m = encontrar_mcm(numero_uno,numero_dos)
+        print(f"El m.c.m. de {numero_uno} y {numero_dos} es {m_c_m}")
+    elif opcion == 2:
+        m_c_d = encontrar_mcd(numero_uno,numero_dos)        
+        print(f"El m.c.d. de {numero_uno} y {numero_dos} es {int(m_c_d)}")
 else:
     print("error")
 
