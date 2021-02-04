028bf93745c3b31ea99eec584f70f1418d154b07
Wilson Gutierrez Lancheros <wilsongutierrezl@gmail.com>
2021-02-04 04:53:01 -0500

    Creación del módulo menu_01

---
+++ dia_13/menu_01.py
@@ -0,0 +1,10 @@
+menu =""" Calculadora de Minimo Común Multiplo y Mínimo Común Divisor
+=========================================
+||      MENU                           ||
+=========================================
+|| Opción || Operación                 ||
+|| 1.     || Mínimo Común Múltiplo     ||
+|| 2.     || Máximo Común Divisor      ||
+||        ||                           ||
+=========================================
+"""
