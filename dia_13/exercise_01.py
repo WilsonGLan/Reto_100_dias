0b2f540bd3b9137942822f4daafe3b7800c3ed75
Wilson Gutierrez Lancheros <wilsongutierrezl@gmail.com>
2021-02-04 01:32:57 -0500

    Refactorizando evaluar_primos()

--- dia_13/exercise_01.py
+++ dia_13/exercise_01.py
@@ -3,23 +3,18 @@ primos_numero_dos = []
 
 #Descomponer los numeros ingresados en factores primos
 
-def evaluar_primos(numero):
-    if numero > 1:
-        contar_divisores = 0
-        for evalua_primo in range(2,numero):
-            if numero > 3 and numero % evalua_primo == 0:
-                contar_divisores += 1
-                return contar_divisores                
-            elif numero - 1 == evalua_primo:
-                return contar_divisores
-
+def evaluar_primos(numero):            
+    for evalua_primo in range(2,numero):
+        if numero > 3 and numero % evalua_primo == 0:            
+            return False                        
+    return True
 
 def lista_primos(numero):
     for i in range(2, numero_uno):
         if numero_uno >= 2 and numero_uno % i == 0 :
             if i in [2,3]:
                 primos_numero_uno.append(i)
-            elif evaluar_primos(i) == 0:
+            elif evaluar_primos(i):
                 primos_numero_uno.append(i)
     return primos_numero_uno
 
@@ -36,12 +31,11 @@ print("""
 """
 )
 
-
 opcion = int(input("Que opción del menú de operaciones selecciona:\t"))
 if opcion in [1,2]:
     numero_uno = int(input("Ingrese el primer número\t"))    
-    #numero_dos = int(input("Ingrese el segundo número\t"))        
+    #numero_dos = int(input("Ingrese el segundo número\t"))     
+    print(lista_primos(numero_uno))   
 else:
     print("error")
 
-print(lista_primos(numero_uno))
