2b1bcb6b4e6c3dc61961b941c2dd32e5635543fb
Wilson Gutierrez Lancheros <wilsongutierrezl@gmail.com>
2021-02-07 01:26:19 -0500

    lista de pokemon con caracteristicas

---
+++ dia_14/data.py
@@ -0,0 +1,33 @@
+pokemon_random = [
+    {
+        "name":"pikachu",
+        "type":"electric",
+        "base_experience": 112
+    },
+    {
+        "name":"charmander",
+        "type":"fire",
+        "base_experience":62
+    },
+    {
+        "name":"bulbasaur",
+        "type":"grass",
+        "base_experience":64
+    },
+    {
+        "name":"pidgeotto",
+        "type":"flying",
+        "base_experience":122
+    },
+    {
+        "name":"zubat",
+        "type":"flying",
+        "base_experience":49
+    },
+    {
+        "name":"mankey",
+        "type":"fighting",
+        "base_experience":61
+    }
+]
+
