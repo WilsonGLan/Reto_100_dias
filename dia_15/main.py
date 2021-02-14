2580695c3d2fd28a85434e9393fed67f85e6805f
Wilson Gutierrez Lancheros <wilsongutierrezl@gmail.com>
2021-02-14 02:42:31 -0500

    Calcula transacción exitosa y dinero sobrante

--- dia_15/main.py
+++ dia_15/main.py
@@ -4,18 +4,20 @@ import data_coffee as data
 
 cost = 0
 turn = True
-apply = True
-coins_entered = {}
 money_entered = 0
+remaining_money = 0
 
 while turn:
-    temporary_quantity ={}    
+    temporary_quantity ={}
+    apply = True        
     order = input("what would you like? (espresso/latte/cappuccino):")
     if order in ['espresso', 'latte', 'cappuccino']:        
         list_ingredients = data.MENU[order]["ingredients"].items()
         cost = data.MENU[order]["cost"]
+        #iterar ingredientes para evaluar si hay suficiente para atender el pedido
         for ingredients, volume in list_ingredients:
-            print(ingredients, "=", data.resources[ingredients])            
+            mesure = 'g' if ingredients == 'coffee' else 'ml'
+            print(f"{ingredients} = {data.resources[ingredients]}{mesure}")            
             if volume > data.resources[ingredients]:
                 print(f"There is not enough {ingredients}")
                 apply = False
@@ -23,26 +25,43 @@ while turn:
                 temporary_quantity[ingredients] = volume
         if apply:
             coin_distribution = data.coins_value.items()
-            print(f"Cost = {cost}")
-            for coin, value_coin in coin_distribution:
+            print(f"Cost = ${cost}")
+            print(f"Money entered = ${money_entered}")
+            print(f"Remaining money = ${remaining_money}")
+            print("==================================================================")
+            #Suma el dinero ingresado para comprar el pedido
+            for coin, value_coin in coin_distribution:                
                 number_coin = int(input(f"How many {coin} you enter \t"))
-                money_entered += (value_coin * number_coin)
-                print(f"Money entered = {money_entered}")            
-            elements_temporary = temporary_quantity.items()
-            for ingredient_temporal, total in elements_temporary:
-                final_total = data.resources[ingredient_temporal] - total
-                data.resources[ingredient_temporal] = final_total
-                #print(ingredient_temporal, "=", data.resources[ingredient_temporal])
+                sum_coin = value_coin * number_coin
+                money_entered += sum_coin
+                money_entered = float("{0:.2f}".format(money_entered))
+                print(f"Coin entered = ${sum_coin}")  
+            money_entered += remaining_money
+            print(f"Money entered = ${money_entered}")  
+            if cost <= money_entered:
+                #calcula cuanto dinero sobra
+                remaining_money = money_entered - cost
+                remaining_money = float("{0:.2f}".format(remaining_money))
+                money_entered = 0#remaining_money
+                elements_temporary = temporary_quantity.items()
+                for ingredient_temporal, total in elements_temporary:
+                    final_total = data.resources[ingredient_temporal] - total
+                    data.resources[ingredient_temporal] = final_total
+                    #print(ingredient_temporal, "=", data.resources[ingredient_temporal])
+            else:
+                print("Sorry that's not enough money. Money refunded.")
                 
-    elif order == 'report':
+    if order == 'report':
         list_report = data.resources.items()
         for ingredients, volume in list_report:
-            print(ingredients, "=", data.resources[ingredients])
-        print(f"Cost = {cost}")
+            mesure = 'g' if ingredients == 'coffee' else 'ml'
+            print(f"{ingredients} = {data.resources[ingredients]}{mesure}")
+        print(f"Cost = ${cost}")
+        print(f"Remaining money = ${remaining_money} ")
 
-    else:
+    if order not in ['espresso', 'latte', 'cappuccino', 'report']:
         print("ha ingresado una opción incorrecta")
-
+    print("==================================================================")    
     opcion = input("Would you like to continue? Type 'on' for yes or 'off' for not\t")
 
     if opcion == 'off':
