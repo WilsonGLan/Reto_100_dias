7dc6474b99f3817641d88e85d93748dd5c80a23a
Wilson Gutierrez Lancheros <wilsongutierrezl@gmail.com>
2021-02-13 23:32:36 -0500

    valida la cantidad de volumen por ingrediente segun la solicitud

--- dia_15/main.py
+++ dia_15/main.py
@@ -7,26 +7,31 @@ milk = 0
 coffee = 0
 cost = 0
 turn = True
-
-# def espresso_total(order_client):
-
+apply = True
 
 while turn:
-
+    temporary_quantity ={}
     order = input("what would you like? (espresso/latte/cappuccino):")
-    if order in ['espresso', 'latte', 'cappuccino']:
-        # print(data.MENU[orden]["ingredients"])
+    if order in ['espresso', 'latte', 'cappuccino']:        
         list_ingredients = data.MENU[order]["ingredients"].items()
         for ingredients, volume in list_ingredients:
-            print(ingredients, "=", volume)
-            # if volume < revisar
+            print(ingredients, "=", data.resources[ingredients])            
+            if volume > data.resources[ingredients]:
+                print(f"There is not enough {ingredients}")
+                apply = False
+            else:
+                temporary_quantity[ingredients] = volume
+        if apply:
+            elements_temporary = temporary_quantity.items()
+            for ingredient_temporal, total in elements_temporary:
+                final_total = data.resources[ingredient_temporal] - total
+                data.resources[ingredient_temporal] = final_total
+                print(ingredient_temporal, "=", data.resources[ingredient_temporal])
+                
     elif order == 'report':
-        water = data.resources["water"] - water
-        milk = data.resources["milk"] - milk
-        coffee = data.resources["coffee"] - coffee
-        print(f"water = {water}")
-        print(f"milk = {milk}")
-        print(f"coffee = {coffee}")
+        list_report = data.resources.items()
+        for ingredients, volume in list_report:
+            print(ingredients, "=", data.resources[ingredients])  
     else:
         print("ha ingresado una opción incorrecta")
 
