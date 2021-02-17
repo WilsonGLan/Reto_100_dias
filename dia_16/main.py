b85ad103bb6bbaec0a33c161b58e9bcc8293f566
Wilson Gutierrez Lancheros <wilsongutierrezl@gmail.com>
2021-02-17 03:59:27 -0500

    probando entorno virtual

--- dia_16/main.py
+++ dia_16/main.py
@@ -1,12 +1,11 @@
-import turtle
+from turtle import Turtle, Screen
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+timmy = Turtle()
 
+my_screen = Screen()
+print(my_screen.canvheight)
+import sys
+print('turtle' in sys.modules)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('wilson')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+for m in sys.modules:
+    print(m)
