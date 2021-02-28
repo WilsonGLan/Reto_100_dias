1659ed07004d01ae8d14ad1f3a33462248127399
Wilson Gutierrez Lancheros <wilsongutierrezl@gmail.com>
2021-02-28 01:48:24 -0500

    metodo still_has_questions
    Se crea un metodo para validar si hay mas preguntas

--- dia_17/quiz_brain.py
+++ dia_17/quiz_brain.py
@@ -3,6 +3,10 @@ class QuizBrain:
         self.question_number = 0
         self.question_list = q_list
 
+    def still_has_question(self):
+        return len(self.question_list) > self.question_number
+
     def nex_question(self):
         current_cuestion = self.question_list[self.question_number]
+        self.question_number += 1
         input(f"Q.{self.question_number}: {current_cuestion.text} (True/False): ")
