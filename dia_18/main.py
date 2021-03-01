4e2fe7c9f45a3b6a8ceb6feecbff4c3fbd30d19d
Wilson Gutierrez Lancheros <wilsongutierrezl@gmail.com>
2021-02-28 23:31:48 -0500

    Dibujar una linea segmentada con la librería turtle

--- dia_18/main.py
+++ dia_18/main.py
@@ -8,15 +8,24 @@ def turtle_square():
     for _ in range(4):
         turtle_right()
 
+def dashed_line(up, down):
+    tim.forward(up)
+    tim.penup() # alza el boligrafo para no dibujar
+    tim.forward(down)
+    tim.pendown() # baja el boligrafo para dibujar
+
 
 tim = Turtle()
 
 tim.shape("turtle")
 tim.color("green")
 
-turtle_square()
+""" Dibuja un cuadrado """
+#turtle_square()
 
-
+""" Dibuja una linea segmentada """
+for _ in range(30):
+    dashed_line(10,10)
 
 
 
