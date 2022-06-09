d3204b3b3e28057c48d2a72362df3d440f2c9058
Wilson Gutiérrez L <wilsongutierrezl@gmail.com>
2022-06-09 01:56:38 -0500

    comenzando proyecto del juego de la culebrita

---
+++ dia_20/proyect_snake.py
@@ -0,0 +1,20 @@
+from turtle import Screen, Turtle
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My snake game")
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
