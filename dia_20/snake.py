fb7e55e6bb6e56e35d3f6b53c42fe72713f25af5
Wilson Gutiérrez L <wilsongutierrezl@gmail.com>
2022-06-12 13:25:38 -0500

    Implementacion del movimiento con el teclado

--- dia_20/snake.py
+++ dia_20/snake.py
@@ -1,15 +1,21 @@
+from tkinter import RIGHT
 from turtle import Turtle
 
 
 STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
 
 class Snake:
 
     def __init__(self):
         self.segments = []
         self.create_snake()
-    
+        self.head = self.segments[0]
+
     def create_snake(self):
         for position in STARTING_POSITIONS:
             new_segment = Turtle("square")
@@ -24,4 +30,20 @@ class Snake:
             new_x = self.segments[seg_nums - 1].xcor()
             new_y = self.segments[seg_nums - 1].ycor()
             self.segments[seg_nums].goto(new_x, new_y)
-        self.segments[0].forward(MOVE_DISTANCE)
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
