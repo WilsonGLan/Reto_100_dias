86e84f30f2b9b5e20694fb0edb2c5cda743c076f
Wilson Gutiérrez L <wilsongutierrezl@gmail.com>
2022-06-14 21:34:53 -0500

    Ejercicio sencillo que intenta explicar la herencia

---
+++ dia_21/exercise.py
@@ -0,0 +1,17 @@
+
+#Probando herencia
+
+class Dog:
+    def __init__(self):
+        self.temperament = "loyal"
+ 
+class Labrador(Dog):
+    def __init__(self):
+        super().__init__()
+        self.temperament = "gentle"
+
+doggo = Dog()
+print(f"A dog is {doggo.temperament}")
+
+sparky = Labrador()
+print(f"Sparky is {sparky.temperament}")
