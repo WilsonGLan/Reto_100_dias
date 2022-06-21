e2be34f73e4ca7d06c0ac4f79768a17b637a1c0b
Wilson Gutiérrez L <wilsongutierrezl@gmail.com>
2022-06-20 21:59:42 -0500

    Se agrega la funcionalidad de detectar cuando la culebra choca contra la pared o la cola y que cuando suceda termine el juego

--- dia_20/proyect_snake.py
+++ dia_20/proyect_snake.py
@@ -1,7 +1,7 @@
 from turtle import Screen
 from snake import Snake
+from scoreboard import Scoreboard
 import time
-
 from food import Food
 
 screen = Screen()
@@ -12,6 +12,7 @@ screen.tracer(0)
 
 snake =Snake()
 food = Food()
+scoreboard = Scoreboard()
 
 screen.listen() # Escucha la pulsacion de las teclas
 screen.onkey(snake.up,"Up")
@@ -24,8 +25,27 @@ game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-
     snake.move()
+
+    #Dectect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+
+    #Dectect collision with wall
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
+
+    #Dectect collision with tail
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
+
 
 
 
