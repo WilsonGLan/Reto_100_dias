4cfdf61b8b6fa23357c86a5d48f13f5534a014d8
Wilson Gutiérrez L <wilsongutierrezl@gmail.com>
2022-07-04 21:57:09 -0500

    Examples to pass several arguments to a function obtaining a tuple or dictionary

--- dia_27/arguments01.py
+++ dia_27/arguments01.py
@@ -1,4 +1,7 @@
 # Probando el paso de argumentos sin limite
+from audioop import mul
+
+
 def add(*args):
     #print(args)
     sum = 0
@@ -9,3 +12,27 @@ def add(*args):
 
 #Todo: Al llamar a la función se pueden pasar la cantidad de argumentos que uno quiera sin error
 add(4,2,3,1,10)
+
+# Pasar argumentos a modo de diccionario
+def calculate(n,**kwargs):
+    ({print(key):print(value) for (key, value) in kwargs.items() if value is not None and key is not None})
+    #print({value for (key, value) in kwargs.items()})
+    # for key, value in kwargs.items():
+    #     print(key)
+    #     print(value)
+    n += kwargs["add"]
+    n *= kwargs["multiply"]
+    print(n)
+
+calculate(3, add=3, multiply=5)
+
+def bar(spam, eggs, toast='yes please!', ham=0):
+    print(spam, eggs, toast, ham)
+ 
+bar(toast='nah', spam=4, eggs=2)
+
+
+def all_aboard(a, *args, **kw): 
+    print(a, args, kw)
+ 
+all_aboard(4, 7, 3, 0, x=10, y=64)
