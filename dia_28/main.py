fb5efb7d81240c942f91eb12396c611f065cab6f
Wilson Gutierrez <wilsongutierrezl@gmail.com>
2022-07-09 20:01:39 -0500

    Agregando funcionalidad al proyecto

--- dia_28/main.py
+++ dia_28/main.py
@@ -14,7 +14,10 @@ LONG_BREAK_MIN = 20
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
-
+def count_down(count):
+    canvas.itemconfig(timer_text, text=count)
+    if count > 0:
+        window.after(1000, count_down, count - 1)
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
@@ -24,8 +27,10 @@ window.config(padx=100,pady=50, bg=YELLOW, highlightthickness=0)
 canvas = Canvas(width=200, height=224, bg=YELLOW)
 tomato_img=PhotoImage(file="tomato.png")
 canvas.create_image(102, 112, image=tomato_img)
-canvas.create_text(103, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+timer_text = canvas.create_text(103, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(column=1, row= 1)
+
+count_down(5)
 
 buttonStart = Button(text="Start", highlightthickness=0)
 buttonStart.grid(column=0, row=3)
