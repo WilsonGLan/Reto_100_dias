6fceaf65ca334efb1c1a707009f2d28d1e0865c4
Wilson Gutierrez <wilsongutierrezl@gmail.com>
2022-07-18 00:54:14 -0500

    El programa permite guardar los datos en un archivo plano

--- dia_29/main.py
+++ dia_29/main.py
@@ -1,10 +1,19 @@
-from email.mime import image
 from tkinter import *
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save():
 
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+
+    with open("data.txt", "a") as data_file:
+        data_file.write(f"{website}|{email}|{password}\n")
+        website_entry.delete(0, END) # Borra todos los caracteres de este objeto
+        #email_entry.delete(0, END)
+        password_entry.delete(0, END)
 # ---------------------------- UI SETUP ------------------------------- #
 
 
@@ -32,15 +41,17 @@ password_label.grid(row=3, column=0)
 
 website_entry = Entry(width=52) # Aumentar o disminuir el width ayuda a alinear cada objeto en el lienzo
 website_entry.grid(row=1, column=1, columnspan=2)
+website_entry.focus() #Cuando se lanza la aplicación el cursor queda en este objeto
 email_entry = Entry(width=52)
 email_entry.grid(row=2, column=1, columnspan=2)
+email_entry.insert(0, "wilsongutierrezl@gmail.com") # Permite establecer un texto predefinido cada vez que inicie la aplicación
 password_entry = Entry(width=33)
 password_entry.grid(row=3, column=1)
 
 #Buttons
 generate_password_button = Button(text="Generate Password")
 generate_password_button.grid(row=3, column=2)
-add_button = Button(text="Add", width=44)
+add_button = Button(text="Add", width=44, command=save)
 add_button.grid(row=4, column=1, columnspan=2)
 
 
