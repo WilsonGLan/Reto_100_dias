3a6b08f79e5ca63e2fa42cafc69c535878b12682
Wilson Gutierrez <wilsongutierrezl@gmail.com>
2022-07-20 22:57:57 -0500

    Agregando validaciones mediante ventanas emergentes

--- dia_29/main.py
+++ dia_29/main.py
@@ -1,5 +1,5 @@
 from tkinter import *
-
+from tkinter import messagebox
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
@@ -9,11 +9,17 @@ def save():
     email = email_entry.get()
     password = password_entry.get()
 
-    with open("data.txt", "a") as data_file:
-        data_file.write(f"{website}|{email}|{password}\n")
-        website_entry.delete(0, END) # Borra todos los caracteres de este objeto
-        #email_entry.delete(0, END)
-        password_entry.delete(0, END)
+    if len(website) == 0 or len(password) == 0:
+        messagebox.showinfo(title="Ooops", message="Please make sure you havent't left any fields empty.")
+    else:
+        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email} "
+                                       f"\nPassword: {password} \nIs it ok to save?")
+        if is_ok:
+            with open("data.txt", "a") as data_file:
+                data_file.write(f"{website}|{email}|{password}\n")
+                website_entry.delete(0, END) # Borra todos los caracteres de este objeto
+                #email_entry.delete(0, END)
+                password_entry.delete(0, END)
 # ---------------------------- UI SETUP ------------------------------- #
 
 
